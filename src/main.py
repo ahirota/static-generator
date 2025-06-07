e62cafe23893b61eed4f009a70158aec21491c4c
ahirota <avjhirota@gmail.com>
2025-06-06 18:26:50 -0700

    [STATIC_2_1] Main Functions for building, comparing, and printing out Text Nodes

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    example = TextNode("Here's some normal text", TextType.TEXT)
+    print(example)
+
+if __name__ == "__main__":
+    main()
