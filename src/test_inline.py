85a345225a5c82d17cdb2850e3c198cf0372262d
ahirota <avjhirota@gmail.com>
2025-06-09 18:05:51 -0700

    [STATIC_3_4] extract links with regex

--- src/test_inline.py
+++ src/test_inline.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from inline import split_nodes_delimiter
+from inline import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestHTMLNode(unittest.TestCase):
     def test_split_nodes_delimiter_code(self):
@@ -29,6 +29,20 @@ class TestHTMLNode(unittest.TestCase):
     def test_split_nodes_delimiter_fail(self):
         node = TextNode("This is text with a `code block` but too many `backticks", TextType.TEXT)
         self.assertRaises(Exception, split_nodes_delimiter, [node], "`", TextType.CODE)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_multiple(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
 if __name__ == '__main__':
     unittest.main()
