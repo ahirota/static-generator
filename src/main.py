408ab96cf61f674b0cfd91113036a2b3054bf909
ahirota <avjhirota@gmail.com>
2025-11-17 20:51:00 -0800

    [RESET] HARD RESET REPO FOR PROJECT START

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from textnode import TextNode, TextType
-
-def main():
-    example = TextNode("Here's some normal text", TextType.TEXT)
-    print(example)
-
-if __name__ == "__main__":
-    main()
