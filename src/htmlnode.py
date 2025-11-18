ca46db832a94d2ea2026f42c34e9f4d760382d04
ahirota <avjhirota@gmail.com>
2025-11-18 13:03:08 -0800

    [SSG_2_5] ParentNode And Unit Tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,21 @@ class LeafNode(HTMLNode):
         
     def __repr__(self):
         return f"LeafNode(Tag: {self.tag}, Value: {self.value}, Props: {self.props})"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("No Tag property found, invalid HTML.")
+        if not self.children:
+            return ValueError("No Children property found, invalid HTML.")
+        else:
+            children_html = ""
+            for child in self.children:
+                children_html += child.to_html()
+            return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -32,6 +32,61 @@ class TestLeafNode(unittest.TestCase):
         node = LeafNode("a", "Click me!", {"href": "https://www.google.com"}).to_html()
         expected = "<a href=\"https://www.google.com\">Click me!</a>"
         self.assertEqual(str(node), expected)
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_multiple_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    def test_to_html_with_props_and_children(self):
+        grandchild_node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        child_node = ParentNode("p", [grandchild_node])
+        parent_node = ParentNode("div", [child_node], {"aria-label": "button"})
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div aria-label=\"button\"><p><a href=\"https://www.google.com\">Click me!</a></p></div>",
+        )
+
+    def test_to_html_with_nested_children(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        p_child_wrapper = ParentNode("p",[
+            LeafNode(None, "raw text here followed by a span"),
+            LeafNode("span", "nested span here", {"prop": "whatisthisdoinghere?"})
+        ])
+        parent_node = ParentNode("div", [
+            child_node,
+            p_child_wrapper
+        ])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span><p>raw text here followed by a span<span prop=\"whatisthisdoinghere?\">nested span here</span></p></div>",
+        )
 
 
 if __name__ == "__main__":
