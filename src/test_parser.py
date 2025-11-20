ece259235e18818f144a1ca1932d5ee522aa60dc
ahirota <avjhirota@gmail.com>
2025-11-19 17:32:54 -0800

    [SSG_3_6] Text to Textnodes And Unit Tests

--- src/test_parser.py
+++ src/test_parser.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from parser import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from parser import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestParser(unittest.TestCase):
     def test_eq(self):
@@ -163,6 +163,63 @@ class TestParser(unittest.TestCase):
                 ),
             ],
             new_nodes,
+        )
+    
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
+
+    def test_text_to_textnodes_multi_bold_no_links(self):
+        text = "This is **text** with another **bold** word and another **bold word** and an image ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) but no links."
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with another ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" word and another ", TextType.TEXT),
+                TextNode("bold word", TextType.BOLD),
+                TextNode(" and an image ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" but no links.", TextType.TEXT),
+            ],
+            nodes,
+        )
+
+    def test_text_to_textnodes_reversed_ordering(self):
+        text = "This is a [link](https://boot.dev) and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a `code block` and an _italic_ word with **bold text** at the end."
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word with ", TextType.TEXT),
+                TextNode("bold text", TextType.BOLD),
+                TextNode(" at the end.", TextType.TEXT),
+            ],
+            nodes,
         )
 
 
