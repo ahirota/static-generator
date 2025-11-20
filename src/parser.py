79cf17d457f2a8b4e0cd35f97e37be2e1265d862
ahirota <avjhirota@gmail.com>
2025-11-19 16:07:20 -0800

    [SSG_3_4] Regex Find Images/Links And Unit Tests

--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -17,3 +18,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     split_nodes.append(TextNode(split[i], text_type))
             new_nodes.extend(split_nodes)
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_parser.py
+++ src/test_parser.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from parser import split_nodes_delimiter
+from parser import split_nodes_delimiter, extract_markdown_links, extract_markdown_images
 
 class TestParser(unittest.TestCase):
     def test_eq(self):
@@ -53,6 +53,18 @@ class TestParser(unittest.TestCase):
             TextNode(" inside.", TextType.TEXT),
         ]
         self.assertEqual(new_nodes, expected)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
 
 if __name__ == "__main__":
